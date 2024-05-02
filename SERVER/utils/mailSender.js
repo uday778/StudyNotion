9b0a05eac266af8f20990a3e56e6fe485ed3f118
uday kumar <uday33379@gmail.com>
2024-05-02 10:41:07 +0530

    chnaged MailSender from:to help desk

--- SERVER/utils/mailSender.js
+++ SERVER/utils/mailSender.js
@@ -11,7 +11,7 @@ const mailSender=async(email,title,body)=>{
         })
 
         let info= await transporter.sendMail({
-            from:'studynotion || codehelp -by babbar',
+            from:'studynotion || Help Desk ',
             to:`${email}`,
             subject:`${title}`,
             html:`${body}`,
